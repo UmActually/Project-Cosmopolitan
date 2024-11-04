f6d2633a7080d3c3a3bdf202b730660cdfe640e6
UmActually <a00832792@tec.mx>
2024-11-04 10:37:32 +0100

    Initial commit

---
+++ Project Cosmopolitan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project Cosmopolitan
+//
+//  Created by Leonardo Corona Garza on 04/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project Cosmopolitan/Project_CosmopolitanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_CosmopolitanApp.swift
+//  Project Cosmopolitan
+//
+//  Created by Leonardo Corona Garza on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_CosmopolitanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
